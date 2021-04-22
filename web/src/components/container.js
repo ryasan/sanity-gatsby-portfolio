10394bb7f8d440c27d75c72e34af2458b010f0be
ryasan86 <ryansantos86@gmail.com>
2021-04-21 22:44:16 -0700

    start landing page

--- web/src/components/container.js
+++ web/src/components/container.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import * as styles from './container.module.css';
+import {cn} from '../lib/helpers';
 
-const Container = ({children}) => <div className={styles.root}>{children}</div>;
+const Container = ({children, className}) => (
+  <div className={cn(styles.root, className)}>{children}</div>
+);
+
+Container.propTypes = {
+  className: PropTypes.string,
+};
 
 export default Container;
--- web/src/components/landing.js
+++ web/src/components/landing.js
@@ -1,7 +1,44 @@
 import React from 'react';
 
+import Container from './container';
 import * as styles from './landing.module.css';
 
-const Landing = () => <div className={styles.root}>Landing Page</div>;
+const Landing = () => (
+  <div className={styles.root}>
+    <Container className={styles.container}>
+      <div className={styles.leftColumn}>{/* <h1>Ryan Santos</h1> */}</div>
+      <div className={styles.rightColumn}>
+        {/* <div className={styles.board}></div> */}
+
+        <div className={styles.myStuff}>
+          <div className={styles.screen}>
+            <div className={styles.screenInnerLandscape}>
+              <div className={styles.screenTrackLandscape}>
+                <div className={styles.codeLandscape}></div>
+                <div className={styles.codeLandscape}></div>
+                <div className={styles.codeLandscape}></div>
+              </div>
+              <div className={styles.frameLandscape}></div>
+            </div>
+            <div className={styles.stand}></div>
+            <div className={styles.standBottom}></div>
+          </div>
+          <div className={styles.screen}>
+            <div className={styles.screenInnerPortrait}>
+              <div className={styles.screenTrackPortrait}>
+                <div className={styles.codePortrait}></div>
+                <div className={styles.codePortrait}></div>
+                <div className={styles.codePortrait}></div>
+              </div>
+              <div className={styles.framePortrait}></div>
+            </div>
+            <div className={styles.stand}></div>
+            <div className={styles.standBottom}></div>
+          </div>
+        </div>
+      </div>
+    </Container>
+  </div>
+);
 
 export default Landing;
