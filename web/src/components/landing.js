1f2f1d8474b593ad90f6ad15c67eb0d7c33145f4
ryasan86 <ryansantos86@gmail.com>
2021-04-22 20:31:23 -0700

    remove board in hero

--- web/src/components/landing.js
+++ web/src/components/landing.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Container from './container';
-import BoardImage from '../static/board.svg';
+// import BoardImage from '../static/board.svg';
 import * as styles from './landing.module.css';
 
 const Landing = () => (
@@ -12,23 +12,11 @@ const Landing = () => (
         <div className={styles.subtitle}>Frontend Developer</div>
       </div>
       <div className={styles.rightColumn}>
-        <div className={styles.board}>
+        {/* <div className={styles.board}>
           <img className={styles.boardImage} src={BoardImage} alt='board' />
-        </div>
+        </div> */}
 
         <div className={styles.myStuff}>
-          <div className={styles.screen}>
-            <div className={styles.screenInnerLandscape}>
-              <div className={styles.screenTrackLandscape}>
-                <div className={styles.codeLandscape}></div>
-                <div className={styles.codeLandscape}></div>
-                <div className={styles.codeLandscape}></div>
-              </div>
-              <div className={styles.frameLandscape}></div>
-            </div>
-            <div className={styles.stand}></div>
-            <div className={styles.standBottom}></div>
-          </div>
           <div className={styles.screen}>
             <div className={styles.screenInnerPortrait}>
               <div className={styles.screenTrackPortrait}>
@@ -37,6 +25,18 @@ const Landing = () => (
                 <div className={styles.codePortrait}></div>
               </div>
               <div className={styles.framePortrait}></div>
+            </div>
+            <div className={styles.stand}></div>
+            <div className={styles.standBottom}></div>
+          </div>
+          <div className={styles.screen}>
+            <div className={styles.screenInnerLandscape}>
+              <div className={styles.screenTrackLandscape}>
+                <div className={styles.codeLandscape}></div>
+                <div className={styles.codeLandscape}></div>
+                <div className={styles.codeLandscape}></div>
+              </div>
+              <div className={styles.frameLandscape}></div>
             </div>
             <div className={styles.stand}></div>
             <div className={styles.standBottom}></div>
