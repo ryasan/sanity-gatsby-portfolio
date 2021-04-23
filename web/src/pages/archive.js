19f7468af1abce5177edafe153d0ecf4fac49a52
ryasan86 <ryansantos86@gmail.com>
2021-04-23 10:35:09 -0700

    edit subtitle front weight

--- web/src/pages/archive.js
+++ web/src/pages/archive.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {graphql} from 'gatsby';
+
 import Container from '../components/container';
 import GraphQLErrorList from '../components/graphql-error-list';
 import ProjectPreviewGrid from '../components/project-preview-grid';
 import SEO from '../components/seo';
 import Layout from '../containers/layout';
 import {mapEdgesToNodes, filterOutDocsWithoutSlugs} from '../lib/helpers';
-
 import {responsiveTitle1} from '../components/typography.module.css';
 
 export const query = graphql`
