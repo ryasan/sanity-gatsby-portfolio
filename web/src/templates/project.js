aec4d225b05f2efb1e58afcb3f4e30d0b9ae2468
ryasan86 <ryansantos86@gmail.com>
2021-04-25 18:12:11 -0700

    fix single project query

--- web/src/templates/project.js
+++ web/src/templates/project.js
@@ -9,7 +9,7 @@ import Layout from '../containers/layout';
 
 export const query = graphql`
   query ProjectTemplateQuery($id: String!) {
-    sampleProject: sanitySampleProject(id: {eq: $id}) {
+    project: sanityProject(id: {eq: $id}) {
       id
       publishedAt
       categories {
@@ -84,8 +84,7 @@ export const query = graphql`
 
 const ProjectTemplate = (props) => {
   const {data, errors} = props;
-  const project = data && data.sampleProject;
-  console.log(data);
+  const project = data && data.project;
   return (
     <Layout>
       {errors && <SEO title='GraphQL Error' />}
