914b0754d315946a4f393435b2550a0babcf44ab
ryasan86 <ryansantos86@gmail.com>
2021-04-28 22:54:54 -0700

    fix overflow in about page

--- studio/schemas/objects/blockContent.js
+++ studio/schemas/objects/blockContent.js
@@ -1,4 +1,4 @@
-import { FaExternalLinkAlt } from 'react-icons/fa'
+import {FaExternalLinkAlt} from 'react-icons/fa';
 
 /**
  * This is the schema definition for the rich text fields used for
@@ -23,19 +23,25 @@ export default {
       // you want and decide how you want to deal with it where you want to
       // use your content.
       styles: [
-        { title: 'Normal', value: 'normal' },
-        { title: 'H1', value: 'h1' },
-        { title: 'H2', value: 'h2' },
-        { title: 'H3', value: 'h3' },
-        { title: 'H4', value: 'h4' },
-        { title: 'Quote', value: 'blockquote' }
+        {title: 'Normal', value: 'normal'},
+        {title: 'H1', value: 'h1'},
+        {title: 'H2', value: 'h2'},
+        {title: 'H3', value: 'h3'},
+        {title: 'H4', value: 'h4'},
+        {title: 'Quote', value: 'blockquote'},
       ],
-      lists: [{ title: 'Bullet', value: 'bullet' }],
+      lists: [{title: 'Bullet', value: 'bullet'}, {title: 'Numbered', value: 'number'}],
       // Marks let you mark up inline text in the block editor.
       marks: {
         // Decorators usually describe a single property – e.g. a typographic
         // preference or highlighting by editors.
-        decorators: [{ title: 'Strong', value: 'strong' }, { title: 'Emphasis', value: 'em' }],
+        decorators: [
+          {title: 'Strong', value: 'strong'},
+          {title: 'Emphasis', value: 'em'},
+          {title: 'Code', value: 'code'},
+          {title: 'Underline', value: 'underline'},
+          {title: 'Strike', value: 'strike-through'},
+        ],
         // Annotations can be any object structure – e.g. a link or a footnote.
         annotations: [
           {
@@ -43,38 +49,38 @@ export default {
             name: 'link',
             type: 'object',
             blockEditor: {
-              icon: FaExternalLinkAlt
+              icon: FaExternalLinkAlt,
             },
             fields: [
               {
                 title: 'URL',
                 name: 'href',
                 type: 'url',
-                validation: Rule =>
+                validation: (Rule) =>
                   Rule.uri({
                     allowRelative: true,
-                    scheme: ['https', 'http', 'mailto', 'tel']
-                  })
+                    scheme: ['https', 'http', 'mailto', 'tel'],
+                  }),
               },
               {
                 title: 'Open in new tab',
                 name: 'blank',
                 description: 'Read https://css-tricks.com/use-target_blank/',
-                type: 'boolean'
-              }
-            ]
-          }
-        ]
-      }
+                type: 'boolean',
+              },
+            ],
+          },
+        ],
+      },
     },
     // You can add additional types here. Note that you can't use
     // primitive types such as 'string' and 'number' in the same array
     // as a block type.
     {
-      type: 'figure'
+      type: 'figure',
     },
     {
-      type: 'slideshow'
-    }
-  ]
-}
+      type: 'slideshow',
+    },
+  ],
+};
