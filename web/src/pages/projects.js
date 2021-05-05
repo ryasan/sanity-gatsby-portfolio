69c2b9ff06e943a1c05e0380976dd29528713d42
ryasan86 <ryansantos86@gmail.com>
2021-05-04 21:19:41 -0700

    filtering for projects

--- web/src/pages/projects.js
+++ web/src/pages/projects.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {graphql} from 'gatsby';
 
 import Container from '../components/container';
+import Categories from '../components/categories';
 import GraphQLErrorList from '../components/graphql-error-list';
 import ProjectPreviewGrid from '../components/project-preview-grid';
 import SEO from '../components/seo';
 import Loader from '../components/loader';
 import {mapEdgesToNodes, filterOutDocsWithoutSlugs} from '../lib/helpers';
-import {responsiveTitle1} from '../components/typography.module.css';
-import {isEmpty} from '../lib/type-check-utils';
+import {responsiveTitle1, responsiveTitle2} from '../components/typography.module.css';
+import {isEmpty, isEmptyArray} from '../lib/type-check-utils';
 
 export const query = graphql`
   query ProjectsPageQuery {
+    categories: allSanityCategory {
+      edges {
+        node {
+          title
+        }
+      }
+    }
     projects: allSanityProject(
       limit: 12
       sort: {fields: [publishedAt], order: DESC}
@@ -31,6 +39,9 @@ export const query = graphql`
           slug {
             current
           }
+          categories {
+            title
+          }
         }
       }
     }
@@ -39,18 +50,40 @@ export const query = graphql`
 
 const ProjectsPage = (props) => {
   const {data, errors} = props;
+  const [activeList, setActiveList] = useState([]);
 
   if (errors) return <GraphQLErrorList errors={errors} />;
   if (isEmpty(data)) return <Loader />;
+
+  const handleClick = (title) => {
+    setActiveList((list) => {
+      if (list.includes(title)) return list.filter((t) => t !== title);
+      else return [...list, title];
+    });
+  };
+
+  const projects = mapEdgesToNodes(data.projects);
+
+  const projectList = !isEmptyArray(activeList)
+    ? projects.filter((project) => {
+        return activeList.some((active) => {
+          return project.categories.some(({title}) => title === active);
+        });
+      })
+    : projects;
 
   return (
     <>
       <SEO title='Side Projects' />
       <Container>
         <h1 className={responsiveTitle1}>Side Projects</h1>
-        <ProjectPreviewGrid
-          nodes={mapEdgesToNodes(data.projects).filter(filterOutDocsWithoutSlugs)}
+        <Categories
+          categories={mapEdgesToNodes(data.categories)}
+          activeList={activeList}
+          onClick={handleClick}
         />
+        {isEmptyArray(projectList) && <h2 className={responsiveTitle2}>Empty 😭</h2>}
+        <ProjectPreviewGrid nodes={projectList.filter(filterOutDocsWithoutSlugs)} />
       </Container>
     </>
   );
