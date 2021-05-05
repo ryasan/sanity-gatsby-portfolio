ac905f937eb0a1a10e2fe0578eec9582863913ee
ryasan86 <ryansantos86@gmail.com>
2021-05-05 13:41:13 -0700

    check mark animation

--- web/src/components/categories/index.js
+++ web/src/components/categories/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import {motion} from 'framer-motion';
 
 import * as styles from './categories.module.css';
 import {withThemeInfo} from '../../context/theme-context';
 import {cn} from '../../lib/helpers';
+
+const CheckMark = () => (
+  <motion.svg className={styles.checkMark} xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 32'>
+    <motion.path
+      className={styles.checkMarkCheck}
+      initial={{strokeDashoffset: 48}}
+      animate={{strokeDashoffset: 0}}
+      transition={{duration: 0.3, delay: 0.3}}
+      d='M4.1 17.2l7.1 7.2 16.7-16.8'
+    />
+  </motion.svg>
+);
 
 const Categories = ({onClick, activeList, categories, isDark}) => (
   <ul className={styles.root}>
@@ -20,7 +33,8 @@ const Categories = ({onClick, activeList, categories, isDark}) => (
           onClick={() => onClick(title)}
           title={title}
           tabIndex={0}>
-          {title.toLowerCase()}
+          {isActive && <CheckMark />}
+          <span>{title.toLowerCase()}</span>
         </li>
       );
     })}
---
+++ web/src/components/icon/checkmark.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CheckmarkIcon = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default CheckmarkIcon
