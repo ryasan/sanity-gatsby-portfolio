a39817f1e4e7c8d1b43b956056bf20c21b5f2f21
ryasan86 <ryansantos86@gmail.com>
2021-05-06 13:55:26 -0700

    edit about

--- web/src/components/skills-experience/index.js
+++ web/src/components/skills-experience/index.js
@@ -4,6 +4,15 @@ import * as styles from './skills-experience.module.css';
 import {responsiveTitle1} from '../typography.module.css';
 import TagCloud from '../tag-cloud';
 
+const paragraphs = [
+  `My primary skills fall mostly under the frontend spectrum of the tech variety. However, I
+   have experience in all areas of the stack and don't mind getting my hands dirty
+   whether it be frontend, backend, or anything in between.`,
+  `I've been part of teams employing scrum methodologies, involved in the feature
+   planning processes, Git rebase and merge workflows...and more. Contact me if you'd
+   like to know more.`,
+];
+
 const SkillsAndExperience = () => (
   <div className={styles.root}>
     <div className={styles.columnContainer}>
@@ -12,16 +21,9 @@ const SkillsAndExperience = () => (
       </div>
       <div className={styles.rightColumn}>
         <h2 className={responsiveTitle1}>Skills &amp; Experience</h2>
-        <p>
-          My primary skills fall mostly under the frontend spectrum of the tech variety. However, I
-          have experience in all areas of the stack and don&apos;t mind getting my hands dirty
-          whether it be frontend, backend, or anything in between.
-        </p>
-        <p>
-          I&apos;ve been part of teams employing scrum methodologies, involved in the feature
-          planning processes, Git rebase and merge workflows...and more. Contact me if you&apos;d
-          like to know more.
-        </p>
+        {paragraphs.map((paragraph, i) => (
+          <p key={i}>{paragraph}</p>
+        ))}
       </div>
     </div>
   </div>
--- web/src/pages/about.js
+++ web/src/pages/about.js
@@ -5,27 +5,27 @@ import Container from '../components/container';
 import SEO from '../components/seo';
 import SkillsAndExperience from '../components/skills-experience';
 
+const paragraphs = [
+  `My name is Ryan Santos and I'm a frontend developer, skater, pool player, and proud
+   father of a chiweenie and french bulldog. I always aim to pursue interests that are
+   meaningful to me and coding is one of the items on that list.`,
+  `I haven't always been interested in software. My career background has actually been
+   rooted in support and hardware. To me, it seemed like a path for the intellectually gifted
+   and I never thought of myself the type.`,
+  `While working as a hardware guy and as time passed, I was gradually exposed to the software
+   side of things. I found development to be more my thing so I switched gears and started a
+   new chapter in my life. Fast forward to today and now I'm into all things development
+   & design!`,
+];
+
 const AboutPage = () => (
   <Container>
     <SEO title='About' />
     <div>
       <h2 className={responsiveTitle1}>Background</h2>
-      <p>
-        My name is Ryan Santos and I&apos;m a frontend developer, skater, pool player, and proud
-        father of a chiweenie and french bulldog. I always aim to pursue interests that are
-        meaningful to me and coding is one of the items on that list.
-      </p>
-      <p>
-        I haven&apos;t always been interested in software. My career background has actually been
-        rooted in support and hardware. To me, it seemed like a path for the intellectually gifted
-        and I never thought of myself the type.
-      </p>
-      <p>
-        While working as a hardware guy and as time passed, I was gradually exposed to the software
-        side of things. I found development to be more my thing so I switched gears and started a
-        new chapter in my life. Fast forward to today and now I&apos;m into all things development
-        &amp; design!
-      </p>
+      {paragraphs.map((paragraph, i) => (
+        <p key={i}>{paragraph}</p>
+      ))}
     </div>
     <SkillsAndExperience />
   </Container>
