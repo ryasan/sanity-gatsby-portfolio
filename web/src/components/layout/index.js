5a8032c5c4b745111619639d003e4299dfd1d897
ryasan86 <ryansantos86@gmail.com>
2021-05-06 15:03:37 -0700

    add scroll top button

--- web/src/components/layout/index.js
+++ web/src/components/layout/index.js
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {motion} from 'framer-motion';
 
 import * as styles from './layout.module.css';
@@ -20,14 +20,35 @@ const LoaderContainer = () => (
 
 const Layout = ({children, onHideNav, onShowNav, showNav, siteTitle}) => {
   const ui = useContext(UIContext);
+  const [isTop, setIsTop] = useState(typeof window !== 'undefined' && window.scrollY === 0);
+  const isMobile = typeof window !== 'undefined' && window.innerWidth < 450;
   const date = new Date().getFullYear();
 
+  const scrollTop = () => {
+    if (typeof window !== 'undefined') {
+      window.scroll({
+        top: 0,
+        behavior: 'smooth',
+      });
+    }
+  };
+
+  const onScroll = () => {
+    if (typeof window !== 'undefined') {
+      if (window.scrollY > 0) setIsTop(false);
+      else setIsTop(true);
+    }
+  };
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      window.addEventListener('scroll', onScroll);
+      return () => window.removeEventListener('scroll', onScroll);
+    }
+  }, []);
+
   return (
-    <div
-      className={cn(
-        styles.root,
-        typeof window !== 'undefined' && window.innerWidth < 450 && showNav && styles.rootMobile,
-      )}>
+    <div className={cn(styles.root, isMobile && showNav && styles.rootMobile)} id='top'>
       {!ui?.uiIsLoaded && <LoaderContainer />}
 
       <motion.div initial={{opacity: 0}} animate={{opacity: 1, transition: {duration: 0.5}}}>
@@ -38,6 +59,18 @@ const Layout = ({children, onHideNav, onShowNav, showNav, siteTitle}) => {
           showNav={showNav}
         />
         <div className={styles.content}>{children}</div>
+        {isMobile && (
+          <motion.a
+            className={styles.scrollTop}
+            animate={{opacity: !isTop ? 1 : 0}}
+            transition={{delay: 0.2}}
+            onClick={scrollTop}>
+            <svg viewBox='0 0 24 24' fill='currentColor'>
+              <path d='M0 0h24v24H0z' fill='none' />
+              <path d='M7 14l5-5 5 5z' />
+            </svg>
+          </motion.a>
+        )}
         <footer className={styles.footer}>
           <div className={styles.footerWrapper}>
             <SocialList />
