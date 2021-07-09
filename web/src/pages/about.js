b7d3a3fc90a135e0a93e9feea0c818538bb990ee
kraferd-ryasan <ryan.santos@kraferd.com>
2021-07-09 09:43:52 -0700

    edit about

--- web/src/pages/about.js
+++ web/src/pages/about.js
@@ -6,16 +6,15 @@ import SEO from '../components/seo';
 import SkillsAndExperience from '../components/skills-experience';
 
 const paragraphs = [
-  `My name is Ryan Santos and I'm a frontend developer, skater, pool player, and proud
-   father of a chiweenie and french bulldog. I always aim to pursue interests that are
-   meaningful to me and coding is one of the items on that list.`,
-  `I haven't always been interested in software. My career background has actually been
-   rooted in support and hardware. To me, it seemed like a path for the intellectually gifted
-   and I never thought of myself the type.`,
-  `While working as a hardware guy and as time passed, I was gradually exposed to the software
-   side of things. I found development to be more my thing so I switched gears and started a
-   new chapter in my life. Fast forward to today and now I'm into all things development
-   & design!`,
+  `My name is Ryan Santos and I'm a frontend developer, pool player, skater, gamer, and proud father
+	 of a chiweenie and french bulldog. I always aim to pursue interests that are meaningful to me and
+	 coding is one of the items on that list.`,
+  `I haven't always been interested in software. My career background has actually been rooted elsewhere
+   in technology. To me, application development just seemed like a path for the intellectually gifted
+   and I never thought of myself as the type.`,
+  `After a while I started to adopt the notion that software was more about belief in yourself more
+	 than anything, even having a big 🧠 (although that probably helps). So I switched gears and started
+   a new chapter. Now I'm into all things development and design!`,
 ];
 
 const AboutPage = () => (
