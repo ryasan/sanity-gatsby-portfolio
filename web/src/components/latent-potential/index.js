fdce11f349302240a4164b97e4f80b2b925db8a9
kraferd-ryasan <ryan.santos@kraferd.com>
2021-07-15 15:15:41 -0700

    edit about me

---
+++ web/src/components/latent-potential/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+import * as styles from './latent-potential.module.css';
+import {small} from '../typography.module.css';
+
+const LatentPotential = () => {
+  return (
+    <div className={styles.root}>
+      <div className={styles.textContainer}>
+        <blockquote className={styles.blockquote}>
+          <div>
+            &quot;When nothing seems to help, I go and look at a stonecutter hammering away at his
+            rock, perhaps a hundred times without as much as a crack showing in it. Yet at the
+            hundred and first blow it will split in two, and I know it was not that last blow that
+            did it&#8212;but all that had gone before.&quot;
+          </div>
+          <div>-Social Reformer Jacob Riis</div>
+        </blockquote>
+        <p>
+          Often times it&apos;s easy feel stagnant when faced with a difficult task. Perhaps
+          you&apos;ve decided to resign because the finish line doesn&apos;t appear to be anywhere
+          in site. Maybe you&apos;ve been trying to learn how to do a kick flip on a skateboard for
+          the past one thousand hours but still haven&apos;t landed a single one. Don&apos;t quit!
+          Progress isn&apos;t how we think it&apos;s suppose to be. Check it out...
+        </p>
+      </div>
+      <figure className={styles.figure}>
+        <img src='https://i.postimg.cc/DZpTXGxJ/potential-2.jpg' alt='plateau' />
+        <figcaption className={small}>
+          <b>Fig. 1</b> - We often expect progress to be linear. At the very least, we hope that it
+          will come quickly. In reality, the results of our efforts are often delayed. It is not
+          until months or years later that we realize the true value of the previous work we have
+          done.
+        </figcaption>
+      </figure>
+      <div className={styles.textContainer}>
+        <p>
+          The diagram above is called the &quot;Plateau of Latent Potential&quot; and it simply
+          states that work is never wasted. It is simply being stored and that sometimes it
+          won&apos;t be until much later that the full value of previous efforts is revealed.
+        </p>
+        <p>
+          Anyways this is something I remind myself constantly now and it always eventually proves
+          to be true. With the correct mind set, we are capable of things we would never expect from
+          ourselves 😄
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default LatentPotential;
--- web/src/pages/about.js
+++ web/src/pages/about.js
@@ -2,30 +2,34 @@ import React from 'react';
 
 import {responsiveTitle1} from '../components/typography.module.css';
 import Container from '../components/container';
+import LatentPotential from '../components/latent-potential/index';
 import SEO from '../components/seo';
 import SkillsAndExperience from '../components/skills-experience';
 
 const paragraphs = [
   `My name is Ryan Santos and I'm a frontend developer, pool player, skater, gamer, and proud father
-	 of a chiweenie and french bulldog. I always aim to pursue interests that are meaningful to me and
+	 of two lovable doggos. I always aim to pursue interests that are meaningful to me and
 	 coding is one of the items on that list.`,
-  `I haven't always been interested in software. My career background has actually been rooted elsewhere
+  `I haven't always been interested in software though. My career background has actually been rooted elsewhere
    in technology. To me, application development just seemed like a path for the intellectually gifted
-   and I never thought of myself as the type.`,
-  `After a while I started to adopt the notion that software was more about belief in yourself more
-	 than anything, even having a big 🧠 (although that probably helps). So I switched gears and started
-   a new chapter. Now I'm into all things development and design!`,
+   and I really just never thought of myself as the type. Nonetheless, I started to put in work and
+   developed new learning habits because it was interesting to me.`,
+  `I tried my best to stop myself from automatically dismissing things as too frustrating or requiring
+   too much effort. Although, there were times where I wondered if I should quit. The imposter syndrome was
+   real and it manifested itself to me in many different shapes and sizes. It was a frequent visitor that always
+   overstayed its welcome.`,
 ];
 
 const AboutPage = () => (
   <Container>
     <SEO title='About' />
     <div>
-      <h2 className={responsiveTitle1}>Background</h2>
+      <h2 className={responsiveTitle1}>Hello!</h2>
       {paragraphs.map((paragraph, i) => (
         <p key={i}>{paragraph}</p>
       ))}
     </div>
+    <LatentPotential />
     <SkillsAndExperience />
   </Container>
 );
