016e8457b4b079388009cc94e032524ca2d91ca6
kraferd-ryasan <ryan.santos@kraferd.com>
2021-07-15 21:26:53 -0700

    edit about

--- web/src/pages/about.js
+++ web/src/pages/about.js
@@ -12,20 +12,20 @@ const AboutPage = () => (
     <div>
       <h2 className={responsiveTitle1}>Hello!</h2>
       <p>
-        My name is Ryan Santos and I&apos;m a frontend developer, pool player, skater, gamer, and
+        My name is Ryan Santos and I&apos;m a frontend engineer, pool player, skater, gamer, and
         proud father of two lovable doggos. I always aim to pursue interests that are meaningful to
-        me and coding is one of the items on that list.
+        me and building stuff with code happens to be one of the items on that list.
       </p>
       <p>
-        I haven&apos;t always been interested in software and my career background has been rooted
-        elsewhere in technology. To me, application development just seemed like a path for the
-        intellectually gifted and I really just never thought of myself as the type. Nonetheless, I
-        started to put in work and developed new learning habits because it was interesting to me.
+        I haven&apos;t always been interested in software though and my career background has
+        actually been rooted elsewhere in technology. To me, application development just seemed
+        like a path for the intellectually gifted and I really just never thought of myself as the
+        type. Nevertheless, there was something fun and interesting about so I went after it.
       </p>
       <p>
         At the beginning it was very easy for me to dismiss things as too frustrating or requiring
         too much effort. There were many times where I wondered if I should quit. The imposter
-        syndrome was real and it was a frequent visitor that always overstayed its welcome.
+        syndrome was real and it was a frequent visitor that always overstayed its freaking welcome.
       </p>
     </div>
     <LatentPotential />
